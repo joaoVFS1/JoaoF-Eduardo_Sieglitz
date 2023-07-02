9b7a89b047d607c8e454be2e11cb9de652b10c01
EduardoSieglitz <133911631+EduardoSieglitz@users.noreply.github.com>
2023-07-02 20:08:16 -0300

    Add files via upload

---
+++ Atividades_1/src/Simples_1.java
@@ -0,0 +1,140 @@
+public class Simples_1 {
+    public static void main(String[] args) {
+
+        System.out.println("1- Meu nome é Eduardo/João Fachin");
+        System.out.println("2- A soma de 2 + 2 é igual a 4");
+        System.out.println("3- **********");
+        System.out.println("4- ");
+        System.out.println("*  *  *  *  *");
+        System.out.println("*           *");
+        System.out.println("*           *");
+        System.out.println("*           *");
+        System.out.println("*           *");
+        System.out.println("*  *  *  *  *");
+        System.out.println("5- ");
+        System.out.println("      # ");
+        System.out.println("    #   #");
+        System.out.println("   #     #");
+        System.out.println("  #        #");
+        System.out.println(" #          #");
+        System.out.println("#  # #  # #  #");
+        System.out.println("6- ");
+        System.out.println("      . ");
+        System.out.println("     . . ");
+        System.out.println("    .   . ");
+        System.out.println("   .     . ");
+        System.out.println("    .   .    ");
+        System.out.println("     . .   ");
+        System.out.println("      .");
+        System.out.println("7- ");
+        System.out.println("            $");
+        System.out.println("         $    $");
+        System.out.println("       $        $");
+        System.out.println("     $           $");
+        System.out.println("    $ $ $ $ $ $ $ $");
+        System.out.println("8- ");
+        System.out.println("         *");
+        System.out.println("      *     *");
+        System.out.println("   *   *   *   * ");
+        System.out.println("     *       *");
+        System.out.println("  *    *    *    *");
+        System.out.println("        *  *");
+        System.out.println("      ***  ***");
+        System.out.println("      ********");
+        System.out.println("9- ");
+        System.out.println("     * * * *       * * * *");
+        System.out.println("   *         *   *          *");
+        System.out.println("   *           *            *");
+        System.out.println("     *                    *");
+        System.out.println("       *                *");
+        System.out.println("         *            *");
+        System.out.println("           *       *");
+        System.out.println("               *");
+        System.out.println("10- ");
+        System.out.println("         .           ");
+        System.out.println("     .       .        ");
+        System.out.println("    .         .     ");
+        System.out.println("     .       .      ");
+        System.out.println("         .        ");
+        System.out.println("11- ");
+        System.out.println("        >");
+        System.out.println("           >");
+        System.out.println("              >");
+        System.out.println("           >");
+        System.out.println("        >");
+        System.out.println("12- ");
+        System.out.println("          <");
+        System.out.println("        <");
+        System.out.println("      <<<<<<<<<<<<<");
+        System.out.println("        <");
+        System.out.println("          <");
+        System.out.println("13- ");
+        System.out.println("                 *  ");
+        System.out.println("               *    *");
+        System.out.println("              *      *");
+        System.out.println("             *        *");
+        System.out.println("         *                *");
+        System.out.println("     *                         *");
+        System.out.println("  *                                *");
+        System.out.println("     *                         *");
+        System.out.println("         *                *");
+        System.out.println("             *         * ");
+        System.out.println("           *       *      *");
+        System.out.println("          *     *    *     *");
+        System.out.println("         *  *            *   *");
+        System.out.println("        *                    *");
+        System.out.println("14- ");
+        System.out.println("       #       #  ");
+        System.out.println("       #       #");
+        System.out.println("  # # # # # # # # # # #");
+        System.out.println("       #       #");
+        System.out.println("       #       #");
+        System.out.println("  # # # # # # # # # # #");
+        System.out.println("       #       #");
+        System.out.println("       #       #");
+        System.out.println("15- ");
+        System.out.println("* * * * * * * * *");
+        System.out.println(" *            *");
+        System.out.println("  *          * ");
+        System.out.println("   *        *   ");
+        System.out.println("    *      *    ");
+        System.out.println("     *    *     ");
+        System.out.println("       **       ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
